56a6718b5909af2ac179d5a47f029c1170bb1af1
Lina Forero <forero.lph@gmail.com>
2026-04-26 00:17:37 +0200

    Add timer HTML structure and styles

--- src/main.js
+++ src/main.js
@@ -1,12 +1,1 @@
 import './style.css'
-
-document.querySelector('#app').innerHTML = `
-<section id="center">
-  <div>
-    <h1>Pomodoro Timer</h1>
-  </div>
-</section>
-
-`
-
-console.log('Hello world');
